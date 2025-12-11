e6c9212542bf861c715a754f6b3285551cdf71af
panther <bennetstenzel7@gmail.com>
2025-12-11 12:33:30 +0100

    day 11 solve

--- 10.py
+++ 10.py
@@ -1,11 +1,11 @@
 import scipy
 def minimize(line):
     _, *bs, js = line.split()
-    bs = [[int(i) for i in button[1:-1].split(',')] for button in bs]
-    js = [int(i) for i in js[1:-1].split(',')]
+    bs = [[int(i) for i in  b[1:-1].split(',')] for b in bs]
+    js =  [int(i) for i in js[1:-1].split(',')]
 
     res = scipy.optimize.linprog(
-        [1 for i in range(len(bs))],
+        [1] * len(bs),
         A_eq = [[int(i in b) for b in bs] for i in range(len(js))],
         b_eq = js,
         integrality = 1
